47c4ab0d5569f85adec1e4d272a24e794b11255e
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-11 13:44:03 +0100

    db test

--- app/database.js
+++ app/database.js
@@ -15,8 +15,9 @@ function connectToDB() {
 	var mysql      = require('mysql');
 	var connection = mysql.createConnection({
 	  host     : '127.0.0.1',
+	  port     : '3306',
 	  user     : 'root',
-	  password : password
+	  password : 'p9TDAcJG'
 	});
 
 	connection.connect(function(err) {
