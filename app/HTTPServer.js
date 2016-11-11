af99a6c31f047b7bf385dc9cae69074d758d3e92
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-11 14:46:22 +0100

    index.html init

--- app/HTTPServer.js
+++ app/HTTPServer.js
@@ -1,17 +1,29 @@
+// http://stackoverflow.com/questions/21955697/variables-between-node-js-server-and-client
+
 //Lets require/import the HTTP module
 var http = require('http');
-var dispatcher = require('httpdispatcher');
+var ejs = require('ejs');
 var fs = require("fs");
+var dispatcher = require('httpdispatcher');
 
 //For all your static (js/css/images/etc.) set the directory name (relative path).
 dispatcher.setStatic('resources');
 
 //A sample GET request    
 dispatcher.onGet("/", function(req, res) {
-    fs.readFile("./www/html/index.html", function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    //since we are in a request handler function
+    //we're using readFile instead of readFileSync
+    fs.readFile('./www/html/index.html', 'utf-8', function(err, content) {
+        if (err) {
+            res.end('error occurred');
+            return;
+        }
+        var temp = 'some temp';  //here you assign temp variable with needed value
+
+        var renderedHtml = ejs.render(content, {temp: temp});  //get redered HTML code
+        res.end(renderedHtml);
     });
 });  
 
