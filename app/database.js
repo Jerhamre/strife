98dc6032b951ff8c0578bc7bfd140e98834cd1e9
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-11 11:53:43 +0100

    db test

--- app/database.js
+++ app/database.js
@@ -15,22 +15,20 @@ var mysql = require('mysql');
 var connection;
 
 function connect() {
-	
+
 	connection = mysql.createConnection({
-		host     : 'localhost',
-		user     : 'root',
-		password : password,
-		database : 'strife_db'
+		host     : 'example.org',
+		user     : 'bob',
+		password : 'secret'
 	});
 
-	connection.connect();
+	connection.connect(function(err) {
+		if (err) {
+			console.error('error connecting: ' + err.stack);
+			return;
+		}
 
-	connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-		if (err) throw err;
-
-	console.log('The solution is: ', rows[0].solution);
+		console.log('connected as id ' + connection.threadId);
 	});
-
-	connection.end();
 }
 
