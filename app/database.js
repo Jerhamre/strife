6974ade07008dbe1c39d3a9cec3f651b3805b09d
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-11 11:30:54 +0100

    db test

--- app/database.js
+++ app/database.js
@@ -1,15 +1,10 @@
-var reader = new FileReader();
-reader.onload = function(progressEvent){
-  // Entire file
-  console.log(this.result);
-
-  // By lines
-  var lines = this.result.split('\n');
-  for(var line = 0; line < lines.length; line++){
-    console.log(lines[line]);
+fs = require('fs')
+fs.readFile('../password', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
   }
-};
-reader.readAsText('../password');
+  console.log(data);
+});
 
 /*
 var mysql      = require('mysql');
