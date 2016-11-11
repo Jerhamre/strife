45bc7b9612113a883cb8c13c20fcca670fa44a4c
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-11 12:26:51 +0100

    db test

--- app/database.js
+++ app/database.js
@@ -12,11 +12,37 @@ fs.readFile('./password', 'utf8', function (err,data) {
 });
 
 function connectToDB() {
-	var Client = require('mysql').Client,
-    client = new Client();
-    client.user = 'strife'; 
-    client.password = password;
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'strife',
+	  password : password
+	});
 
-    client.connect();
+	connection.connect(function(err) {
+	  if (err) {
+	    console.error('error connecting: ' + err.stack);
+	    return;
+	  }
+
+	  console.log('connected as id ' + connection.threadId);
+	});
+	/*var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'strife',
+	  password : password,
+	  database : 'strife_db'
+	});
+
+	connection.connect();
+
+	connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+	  if (err) throw err;
+
+	  console.log('The solution is: ', rows[0].solution);
+	});
+
+	connection.end();*/
 }
 
