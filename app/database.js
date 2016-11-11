66196a4f2cd5b54a455271ab0aa80a5afeaff76a
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-11 11:43:32 +0100

    db test

--- app/database.js
+++ app/database.js
@@ -1,4 +1,4 @@
-var password = '';
+var password;
 
 fs = require('fs')
 fs.readFile('./password', 'utf8', function (err,data) {
@@ -6,26 +6,33 @@ fs.readFile('./password', 'utf8', function (err,data) {
     return console.log(err);
   }
   console.log(data);
-  password = String(data);
-  console.log("pass again: " + password);
+  password = data;
+
+  connect();
+  test();
 });
 
+var mysql = require('mysql');
+var connection;
 
-var mysql      = require('mysql');
-console.log("pass again again: " + password);
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : password,
-  database : 'strife_db'
-});
+function connect() {
+	connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : password,
+		database : 'strife_db'
+	});
+}
 
-connection.connect();
+function test() {
+	connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
+	connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+		if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
-});
+	console.log('The solution is: ', rows[0].solution);
+	});
 
-connection.end();
+	connection.end();
+}
+
