2c5c9492f076d32b52c708f22132d4d2fddebf75
root <root@unbunserver01.skelabb.ltu.se>
2016-11-15 22:09:26 +0100

    updated css

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
 // index.js
-
+//http://stackoverflow.com/questions/4018154/node-js-as-a-background-service
+//https://certsimple.com/blog/deploy-node-on-linux#node-linux-service-systemd
 require('./app/index')  
 require('./app/database')
 require('./app/HTTPServer')
