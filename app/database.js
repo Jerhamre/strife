ce7f6ffd9bdb5d94240a0aecf974ba2f062eef05
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-17 14:47:11 +0100

    added error handling for connection of db

--- app/database.js
+++ app/database.js
@@ -22,6 +22,13 @@ function connectToDB() {
 	  database : 'strife_db'
 	});
 
+	connection.connect(function(err) {
+		if (err) {
+			console.error('error connecting: ' + err.stack);
+			return;
+		}
+		console.log('connected as id ' + connection.threadId);
+	});
 }
 
 function query(sql, data) {
