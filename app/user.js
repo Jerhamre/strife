1d082cf21244331accae991c6c1776b11d504ca1
Jerhamre <anton.jerhamre@gmail.com>
2016-11-25 12:34:02 +0100

    database

--- app/user.js
+++ app/user.js
@@ -31,9 +31,11 @@ User.prototype.login = function(email, password, session_user_id, res) {
 		console.log('hash: ' + hash);
 		console.log('pass: ' + result['password']);
 
-		if(hash == result['password'])
+		if(hash == result['password']){
 			session_user_id = 1;
-		console.log('session_user_id = 1 ');
+			console.log('session_user_id = 1 ');
+		}
+		console.log(session_user_id);
 
 		if(session_user_id) {
 	        res.redirect('/');
