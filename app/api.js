ebda27adc8aef9426e2e650c662793c9487de487
Jerhamre <anton.jerhamre@gmail.com>
2016-11-30 11:58:51 +0100

    testing friendlist

--- app/api.js
+++ app/api.js
@@ -1,5 +1,10 @@
+var db = null;
+var server = null;
 
-
+function Api(db_in, server_in) {
+	db = db_in;
+	server = server_in;
+};
 
 
 Api.prototype.sendResponse = function(result, res) {
@@ -7,3 +12,5 @@ Api.prototype.sendResponse = function(result, res) {
 	res.send(JSON.stringify(result));
 
 };
+
+module.exports.Api = Api
--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 //https://certsimple.com/blog/deploy-node-on-linux#node-linux-service-systemd
 require('./app/index')  
 var db 		= require('./app/database')
+var api 	= require('./app/api')
 var user 	= require('./app/user')
 var server 	= require('./app/HTTPServer')
 
+api = new api.Api(db,server)
 user = new user.User(db, server)
 
 server.startServer(db, user)
