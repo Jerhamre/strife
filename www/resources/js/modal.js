1adcf5f504ef1ccbf22ce891806c84bafec9b710
henrik_nh <henrik.nilsson.harnert@gmail.com>
2016-11-30 12:01:46 +0100

    added first version of modals

---
+++ www/resources/js/modal.js
@@ -0,0 +1,35 @@
+// Get the modal
+var currentActiveModal;
+
+function showModal() {
+    currentActiveModal.style.display = "block";
+    centerModal()
+}
+
+function closeModal() {
+    currentActiveModal.style.display = "none";
+}
+
+function centerModal() {
+	console.log(currentActiveModal.style.height)
+	var modalContent = $(currentActiveModal).find('.modal-content');
+	console.log(modalContent.outerHeight());
+	modalContent.css('margin-top', -modalContent.outerHeight()/4*3);
+
+}
+
+window.onclick = function(event) {
+    if (event.target == currentActiveModal) {
+        currentActiveModal.style.display = "none";
+    }
+}
+
+function click_newfriend() {
+	currentActiveModal = document.getElementById('modal_newfriend');
+	showModal();
+}
+
+function click_handleservers() {
+	currentActiveModal = document.getElementById('modal_handleservers');
+	showModal();
+}
