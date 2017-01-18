a306838473c825138abf3eca887dedeed91ead6e
Anton Jerhamre <anton.jerhamre@gmail.com>
2017-01-18 18:59:04 +0100

    Create install_dependancies.sh
    A script that installs all npm dependancies

---
+++ install_dependancies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+npm install mysql
+npm install express
+npm install express-session
+npm install body-parser
+npm install socket.io
+npm install socket.io-express-session
+npm install session-file-store
+npm install multer
