89cbbc1e3b7442ba2cd2a9d315b3d7474afdb0d2
Henrik Nilsson Harnert <henrik.nilsson.harnert@gmail.com>
2017-01-20 12:19:03 +0100

    Update database.js

--- app/database.js
+++ app/database.js
@@ -17,9 +17,9 @@ var connection = null;
 function connectToDB() {
 
 	connection = mysql.createConnection({
-	  host     : 'cloud-59.skelabb.ltu.se',//'127.0.0.1',
+	  host     : 'localhost',
 	  port     : '3306',
-	  user     : 'strife',
+	  user     : 'root',
 	  password : password,
 	  database : 'strife_db'
 	});
